30afa4f283203a3c4a0f37b8f814b4934ad18791
hndrk <hndrkrms@gmail.com>
2024-02-06 00:12:05 -0400

    fix ui session updateprefab

--- Assets/Code/UI/Menu/UIMultiplayer.cs
+++ Assets/Code/UI/Menu/UIMultiplayer.cs
@@ -12,22 +12,26 @@ namespace CatGame.UI
         [SerializeField]
         private Button _createSessionBtn;
         [SerializeField]
+        private Button _joinSessionBtn;
+        [SerializeField]
         private GameObject _sessionContainer;
         [SerializeField]
-        UISessionInfo _sessionPrefab;
+        private UISessionInfo _sessionPrefab;
 
 
         private List<SessionInfo> _sessionInfo = new List<SessionInfo>(32);
         private SessionInfo _selectSession;
-
+        private int _sessionSelectIndex = 0;
         protected override void OnInitialize()
         {
             base.OnInitialize();
             _createSessionBtn.onClick.AddListener(OnCreateGameButton);
+            _joinSessionBtn.onClick.AddListener(OnJoinButton);
         }
         protected override void OnDeinitialize()
         {
             _createSessionBtn.onClick.RemoveListener(OnCreateGameButton);
+            _joinSessionBtn.onClick.RemoveListener(OnJoinButton);
             base.OnDeinitialize();
         }
         protected override void OnOpen()
@@ -75,18 +79,38 @@ namespace CatGame.UI
         {
             
         }
+        private void OnJoinButton() 
+        {
+            if (CanJoinSession(_selectSession))
+                return;
+            Context.Matchmaking.JoinSession(_selectSession);
+        }
+        private bool CanJoinSession(SessionInfo session) 
+        {
+            if (session == null)
+                return false; 
+            
+            return true;
+        }
         private void UpdateSessionListUI() 
         {
             var sessions = _sessionContainer.GetComponentsInChildren<UISessionInfo>();
             for (int i = 0; i < sessions.Length; i++)
             {
+                sessions[i].OnBtnSelect -= SlectSession;
                 Destroy(sessions[i]);
             }
             for (int i = 0; i < _sessionInfo.Count; i++)
             {
                 var sessioninstance = Instantiate(_sessionPrefab,_sessionContainer.transform);
-                sessioninstance.SetData(_sessionInfo[i].Name);
+                sessioninstance.SetData(_sessionInfo[i].Name,i);
+                sessioninstance.OnBtnSelect += SlectSession;
             }
+        }
+        private void SlectSession(int index) 
+        {
+            _sessionSelectIndex = index;
+            _selectSession = _sessionInfo[_sessionSelectIndex];
         }
     }
 }
--- Assets/Code/UI/Menu/UISessionInfo.cs
+++ Assets/Code/UI/Menu/UISessionInfo.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CatGame.UI
 {
@@ -9,9 +12,22 @@ namespace CatGame.UI
     {
         [SerializeField]
         private TextMeshProUGUI _content;
-        public void SetData(string info) 
+        [SerializeField]
+        private Button _selectBtn;
+        public event Action<int> OnBtnSelect;
+        public int Index;
+        public void Awake()
+        {
+            _selectBtn.onClick.AddListener(OnSelectBtn);
+        }
+        public void OnSelectBtn() 
+        {
+            OnBtnSelect(Index);
+        }
+        public void SetData(string info, int index) 
         {
             _content.text = info;
+            Index = index;
         }
     }
 }
